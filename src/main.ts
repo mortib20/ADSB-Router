1b07d584df7b4ecfdaf327f3f02717fa28af53ab
Martin Paul <martin.paul-3277@web.de>
2023-07-12 19:01:31 +0200

    Works even better now

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-import Server, { Socket } from 'net';
+import Server, { Socket, createConnection, createServer } from 'net';
 import { createLogger, format, transports } from 'winston';
 
-interface FeedOutput {
+interface Output {
     name: string;
     host: string;
     port: number;
 }
+
+const port = 30004;
 
 /* Logger */
 const logger = createLogger({
@@ -14,7 +16,7 @@ const logger = createLogger({
 });
 
 /* Network */
-const feedOutputs: FeedOutput[] = [
+const outputsList: Output[] = [
     { name: 'ADSB.lol', host: 'feed.adsb.lol', port: 30004 },
     { name: 'ADSB.fi', host: 'feed.adsb.fi', port: 30004 },
     { name: 'ADSB.one', host: 'feed.adsb.one', port: 64004 },
@@ -22,47 +24,63 @@ const feedOutputs: FeedOutput[] = [
     { name: 'ADSB.Planespotters', host: 'feed.planespotters.net', port: 30004 }
 ];
 
-const outputs: Socket[] = ConnectOutputs(feedOutputs);
+const outputs = CreateOutputs(outputsList);
 
-const receiver = Server.createServer({ keepAlive: true });
-
-receiver.listen({ port: 30004 }, () => {
-    logger.info('ADSB-Router listening on port :30004');
-});
-
-receiver.on('connection', ReceiverListener);
-receiver.on('close', () => {
-    logger.info('Server closed');
-});
-
-function ReceiverListener(socket: Socket) {
-    const { remoteAddress, remotePort } = socket;
-
-    logger.info(`INPUT: ${remoteAddress}:${remotePort} connected`);
+const input = createServer({ keepAlive: true, allowHalfOpen: true }, (socket) => {
+    var address = `${socket.remoteAddress}:${socket.remotePort}`;
+    logger.info(`INPUT: ${address} connected`);
 
     socket.on('data', (data) => {
-        outputs.forEach((output) => output.write(data));
+        outputs.forEach((socket) => {
+            socket.write(data);
+        })
     });
 
     socket.on('close', (hadError) => {
-        logger.info(`INPUT: ${remoteAddress}:${remotePort} disconnected error: ${hadError}`);
+        if (hadError) {
+            logger.error(`INPUT: ${address} connection closed`);
+        } else {
+            logger.info(`INPUT: ${address} connection closed`);
+        }
     });
+})
 
-    socket.on('error', (err) => {
-        logger.error(err.message);
+input.on('error', (err) => {
+    logger.error(`INPUT: ${err.message}`);
+})
+
+input.listen(port, () => logger.info(`ADSB-Router listening on port :${port}`));
+
+function CreateOutputs(list: Output[]) {
+
+    return list.map((output) => {
+        var socket = createConnection({ host: output.host, port: output.port, keepAlive: true, keepAliveInitialDelay: 60000, allowHalfOpen: true });
+
+        socket.on('connect', () => {
+            logger.info(`OUTPUT: ${output.name} connected`);
+        })
+
+        socket.on('error', (err) => {
+            logger.error(`OUTPUT: ${output.name} -> ${err.message}`);
+        })
+
+        socket.on('close', (hadError) => {
+            if(hadError) {
+                logger.info(`Reconnecting to ${output.name}`);
+                socket.connect({port: output.port, host: output.host, keepAlive: true})
+                return;
+            }
+
+            logger.info(`OUTPUT: ${output.name} closed`);
+        })
+
+        return socket;
     });
 }
 
-function ConnectOutputs(feedOutputs: FeedOutput[]): Socket[] {
-    return feedOutputs.map((output) => {
-        const { name, host, port } = output;
-        const out = Server.createConnection(port, host, () => {
-            logger.info(`OUTPUT: Connected to ${name}`);
-        });
-
-        out.on('error', (err) => logger.error(err.message));
-        out.on('close', (hadError) => logger.info(`OUTPUT: Disconnected from ${name} error: ${hadError}`));
-
-        return out;
-    });
-}
+/**
+ * 1. Start Server
+ * 2. Enable Outputs
+ * 3. Server received data => send to Outputs
+ * 4. Reconnect after error
+ */
